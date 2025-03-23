a4d8b5e67c559528c47b51b7b361521281394503
Digital <73296205+digital8150@users.noreply.github.com>
2025-03-23 16:23:15 +0900

    feature: 프로퍼티 실습

---
+++ BCSD Game Beginner CSharp Exercise/Classes/PropertyExercise.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BCSD_Game_Beginner_CSharp_Exercise.Classes
+{
+    internal class PropertyExercise
+    {
+        private string _name = "name";
+        private int _value;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public int Value
+        {
+            get { return _value; }
+            set { _value = value; }
+        }
+    }
+
+    class AutoProperty
+    {
+        public int CurrentHP
+        {
+            get; private set;
+        }
+
+        public string ID
+        {
+            get; set;
+        }
+    }
+
+    class PropertyInit
+    {
+        public void Init()
+        {
+            PropertyExercise propertyExercise = new PropertyExercise()
+            {
+                Name = "name",
+                Value = 1
+            };
+        }
+    }
+}
